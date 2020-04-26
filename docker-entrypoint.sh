d72253269a222a4cb7657b69f618820dbba9948a
Andrew Johnson <blackandred@users.noreply.github.com>
2020-04-26 15:55:54 +0200

    #35: Add support for Docker secrets

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,28 @@
 
 set -e
 
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
 correct_permissions () {
     echo " >> Setting user id and group id"
     usermod -u "$TAIGA_UID" taiga
@@ -70,6 +92,12 @@ migrate() {
 
     /opt/riotkit/bin/plugin-manager.py after-migrations
 }
+
+file_env 'TAIGA_DB_PASSWORD'
+file_env 'TAIGA_SECRET_KEY'
+file_env 'TAIGA_BROKER_URL'
+file_env 'LDAP_BIND_PASSWORD'
+file_env 'TAIGA_EMAIL_PASS'
 
 correct_permissions
 prepare_configs
